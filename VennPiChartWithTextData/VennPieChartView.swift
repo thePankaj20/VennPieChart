987dba974e89ce5e4c44fa10b22d7a417be2f3ce
Nilaakash Singh <nilaakash.singh@sts.in>
2019-01-04 11:38:26 +0530

    extra comment removed

--- VennPiChartWithTextData/VennPieChartView.swift
+++ VennPiChartWithTextData/VennPieChartView.swift
@@ -183,12 +183,5 @@ class VennPieChartView: UIView {
         textLayer.contentsScale = UIScreen.main.scale
         return textLayer
     }
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
 
 }
